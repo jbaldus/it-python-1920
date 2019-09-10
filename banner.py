e9c06784134406e32f189ffd7b1cd1351085d13b
Jason Baldus <jason.baldus@gmail.com>
2019-09-10 12:03:13 -0400

    Adds three programs

---
+++ banner.py
@@ -0,0 +1,17 @@
+
+def banner(subject, author):
+    subject_length = len(subject)
+    by_line = f"By {author}"
+    by_line_length = len(by_line)
+    banner_length = max(subject_length, by_line_length) + 4
+    print(f"{'=' * banner_length}")
+    print(f"{subject:^{banner_length}}")
+    print(f"{by_line:^{banner_length}}")
+    print(f"{'=' * banner_length}")
+    print("")
+
+if __name__ == "__main__":
+    banner("BANNER", "Mr. Baldus")
+    subject = input("What's the subject?")
+    author = input("Who's the author?")
+    banner(subject, author)
