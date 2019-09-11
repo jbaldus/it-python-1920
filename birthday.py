88b9bee996dcf72946994b0d886efae3a800e782
Jason Baldus <jason.baldus@gmail.com>
2019-09-11 12:59:28 -0400

    Starts birthday.py

--- birthday.py
+++ birthday.py
@@ -1,3 +1,28 @@
 from banner import banner
 
 banner("BIRTHDAY", "Mr. Baldus")
+
+# 1. Find out when you were born
+# 2. Figure out how many days until your birthday
+# 3. Print information about the birthday: Days until, Days ago, or Happy B-Day
+
+def main():
+    get_birthday_info()
+    compute_days_between_dates()
+    print_birthday_info()
+
+def get_birthday_info():
+    print("When were you born?")
+    year = input("Year [YYYY]: ")
+    month = input("Month [MM]: ")
+    day = input("Day [DD]: ")
+
+def compute_days_between_dates():
+    pass
+
+def print_birthday_info():
+    pass
+
+
+
+main()
