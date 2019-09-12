42c4bfe4687fa655685d869deda48415c3f9ac5f
Jason Baldus <jason.baldus@gmail.com>
2019-09-12 14:36:29 -0400

    Completes the print_birthday_info function

--- birthday.py
+++ birthday.py
@@ -14,6 +14,7 @@ def main():
     num_days = calculate_days_between_dates(birthday, now)
     print_birthday_info(num_days)
 
+
 def get_birthday_from_user():
     print("What is your birthday?")
     year = int(input("Year [YYYY]? "))
@@ -23,28 +24,20 @@ def get_birthday_from_user():
     birthday = datetime.date(year, month, day)
     return birthday
 
+
 def calculate_days_between_dates(date1, date2):
     this_year = datetime.date(date2.year, date1.month, date1.day)
     dt = this_year - date2
     return dt.days
 
 
-
-
-
-
-
-
-
-
-
-
-
 def print_birthday_info(number_of_days):
-    print(number_of_days)
-
-
-
+    if number_of_days > 0:
+        print(f"Your birthday is in {number_of_days} days. Can't wait!")
+    elif number_of_days < 0:
+        print(f"Your birthday was {-number_of_days} days ago. Can't you remember?")
+    else:
+        print("Today's your birthday! Happy Birthday!")
 
 
 main()
