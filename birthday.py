183d9640c1ccf988c2c750dd1a9eb6cb822d0335
Jason Baldus <jason.baldus@gmail.com>
2019-09-13 09:31:09 -0400

    Two blank lines after each function definition

--- birthday.py
+++ birthday.py
@@ -13,6 +13,7 @@ def main():
     num_days = compute_days_between_dates(birthday, today)
     print_birthday_info(num_days)
 
+
 def get_birthday_info():
     print("When were you born?")
     year = int(input("Year [YYYY]: "))
@@ -22,10 +23,12 @@ def get_birthday_info():
     birthday = datetime.date(year, month, day)
     return birthday
 
+
 def compute_days_between_dates(original_date, target_date):
     current_year = datetime.date(target_date.year, original_date.month, original_date.day)
     dt = current_year - target_date
     return dt.days
+
 
 def print_birthday_info(number_of_days):
     if number_of_days == 0:
